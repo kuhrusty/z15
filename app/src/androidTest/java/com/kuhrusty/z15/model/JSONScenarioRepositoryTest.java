d08114318e599276094886943dfb102dedd457a5
Rusty Ballinger <rusty.ballinger@gmail.com>
2019-06-01 00:04:29 -0700

    Fiddle with prefs on the target device, as they affect the test.

--- app/src/androidTest/java/com/kuhrusty/z15/model/JSONScenarioRepositoryTest.java
+++ app/src/androidTest/java/com/kuhrusty/z15/model/JSONScenarioRepositoryTest.java
@@ -1,8 +1,14 @@
 package com.kuhrusty.z15.model;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.kuhrusty.z15.SettingsActivity;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,6 +18,28 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class JSONScenarioRepositoryTest {
+    private boolean saved_SHOW_KS_SCENARIOS = false;
+
+    @Before
+    public void fiddlePrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
+                InstrumentationRegistry.getTargetContext());
+        if (prefs != null) {
+            saved_SHOW_KS_SCENARIOS = prefs.getBoolean(
+                    SettingsActivity.PREF_SHOW_KS_SCENARIOS, saved_SHOW_KS_SCENARIOS);
+            prefs.edit().putBoolean(SettingsActivity.PREF_SHOW_KS_SCENARIOS, true).commit();
+        }
+    }
+
+    @After
+    public void unfiddlePrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
+                InstrumentationRegistry.getTargetContext());
+        if (prefs != null) {
+            prefs.edit().putBoolean(SettingsActivity.PREF_SHOW_KS_SCENARIOS, saved_SHOW_KS_SCENARIOS).commit();
+        }
+    }
+
     @Test
     public void testRead() {
         ScenarioRepository sr = new JSONScenarioRepository(
@@ -20,7 +48,7 @@ public class JSONScenarioRepositoryTest {
         List<Scenario> scenarios = sr.getScenarios();
         //  Well, this is awkward.  Whether or not this test passes depends on
         //  whether the pref_show_ks_scenarios pref is set on the target device!
-        assertEquals(15, scenarios.size());
+        assertEquals(20, scenarios.size());
         check(scenarios.get(1), "2", "The End of Innocence - Part 2",
                 Scenario.Difficulty.Prologue, Shufflers.Scenario2,
                 Soundtrack.Type.Growl60, 1);
